bddb257d9ac9f44be2b3b05a328f70f9e7f27cd5
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2021-04-26 14:53:12 +0530

    Add files via upload

---
+++ ibm_code.py
@@ -0,0 +1,30 @@
+import wiotp.sdk.device
+import time
+import random
+myConfig = { 
+    "identity": {
+        "orgId": "hj5fmy",
+        "typeId": "NodeMCU",
+        "deviceId":"12345"
+    },
+    "auth": {
+        "token": "12345678"
+    }
+}
+
+def myCommandCallback(cmd):
+    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+    m=cmd.data['command']
+
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+    temp=random.randint(-20,125)
+    hum=random.randint(0,100)
+    myData={'temperature':temp, 'humidity':hum}
+    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+    print("Published data Successfully: %s", myData)
+    client.commandCallback = myCommandCallback
+    time.sleep(2)
+client.disconnect()
